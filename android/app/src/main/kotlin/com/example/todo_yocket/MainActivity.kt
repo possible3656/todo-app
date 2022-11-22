dee78f7db6eea3636559d2e7a17d436f671c2078
sambhav <sj94611@gmail.com>
2022-11-22 16:23:09 +0530

    intial commit

---
+++ android/app/src/main/kotlin/com/example/todo_yocket/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.todo_yocket
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/data/res/color_res.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorRes {
+  static Color primaryBackground = Color(0xff0e1424);
+  static Color secondaryBackground = Color(0xff1d2332);
+  static Color primaryText = Color(0xffcfd6dc);
+  static Color secodaryText = Color(0xff888f9a);
+}
